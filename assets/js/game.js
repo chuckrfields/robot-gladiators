cde4050a083dd61cd9dc496f233b11ca1c84ba41
Chuck Fields <info@spacetechcorp.com>
2021-04-17 11:34:08 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -89,14 +89,18 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
          }
 
+         // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack -3, playerAttack);
+
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
     
         // check enemy's health
@@ -114,7 +118,9 @@ var fight = function(enemyName) {
         }
     
         // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0,  playerHealth - damage);
+
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
     
         // check player's health
@@ -144,9 +150,9 @@ var startGame = function() {
         // pick new enemy to fight based on the index of the enemyNames array
         //local scope
         var pickedEnemyName = enemyNames[i];
-  
-        // reset enemyHealth before starting new fight
-        enemyHealth = 50;
+         
+        // reset enemyHealth before starting new fight 
+        enemyHealth = randomNumber(40, 60);  
   
         // use debugger to pause script from running and check what's going on at that moment in the code
         // debugger;
@@ -241,6 +247,12 @@ var shop = function() {
             shop();
             break;
     }
+}
+
+// Return random number between 0 and 1 multiplied by (max - min + 1) then add min, so we end up with number between min and max
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
 }
 
 // Start the game when the page loads
